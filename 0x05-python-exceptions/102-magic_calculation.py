d8bc981215d4231a487258155c0369223c897878
Behailu Adugna <behailuadugna0@gmail.com>
2022-09-13 05:47:06 +0300

    fixing

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -10,5 +11,3 @@ def magic_calculation(a, b):
             result = b + a
             break
     return result
-
-
